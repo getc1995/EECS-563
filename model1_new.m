002b6897c0920445a2210feb63ded59ff21c86d6
ys <lixhuang@umich.edu>
2018-12-10 04:16:22 -0500

    model1_new and model2_new are the one considering partion. model2 nothing feasible, model1 has feasible initialization though looks bad

--- model1_new.m
+++ model1_new.m
@@ -18,30 +18,28 @@ opts = sdpsettings('solver','sedumi','sos.model',2,'sos.scale',1,'verbose',1,...
 
 % system
 % x = Ax+B(u-Ed);
-A = [zeros(2),eye(2);zeros(2),zeros(2)];
-B = [zeros(2);eye(2)];
-E = [-1;0];
-
+A = zeros(2);
+B = eye(2);
+E = [-1; 0];
 % Constants
 Lx = 6; % meters
 Ly = 3; % meters
-uxmax = 50; % m/s, longitudinal acceleration
-uxmin = -50; % m/s
-uymax = 10; % m/s, lateral acceleration
-uymin = -10; % m/s
-dumax = 1; % m/s
-dumin = -1; % m/s
-vxmax = 5; % m/s, longitudinal velocity
-vymax = 1; % m/s, lateral velocity
+Lx = 6; % meters
+Ly = 3; % meters
+umax = 20; % m/s, longitudinal velocity
+umin = 10; % m/s
+um = 15;
+dumax = 16; % m/s
+dumin = 14; % m/s
+vmax = 1; % m/s, lateral velocity
 
 % Variables
 % let x = x_e - x_o, x is x diff
 % let y = y_e - y_o, h is y diff
-% u = sKx
-sdpvar px py vx vy ux uy d 
-x = [px; py; vx; vy];
-u = [ux;uy];
-fx = A*x + B*(u+E*d);
+% u = -Kx
+sdpvar px py d 
+x = [px; py];
+%fx = A*x + B*(u+E*d);
 
 % minimize k to maximize the volume of B >= 0.
 sdpvar k
@@ -57,8 +55,8 @@ safe_set1 = 1 - (px)^2/(10)^2 - (py-6)^2/(4)^2; % >= 0 real safe
 init_set = 3^2 - (px+17)^2 - (py-1.5)^2;
 
 initial_X = safe_set1 - 0.99;
-K = -lqr(A,B,0.1*eye(4),100*eye(2));
-gamma = 5;
+K = -lqr(A,B,0.1*eye(2),100*eye(2));
+gamma = 40;
 eps = 1e-7;
 
 % Non-empty set constraint
@@ -78,38 +76,22 @@ safe_const1 = -(Barrier + eps) + s21*safe_set1; % >= 0, such that B in safe, or 
 % Control barrier constraint
 [s3, c3, m3] = polynomial([x;d],2);
 [s4, c4, m4] = polynomial([x;d],2);
-xd = [0;6;0;0];
-den1 = 1 + (K(1,:)*(x-xd)/(2*uxmax))^2;
-den2 = 1 + (K(2,:)*(x-xd)/(2*uymax))^2;
-u = K*(x-xd);
-control_const = (dB * (A*x + B*E*d) * den1*den2 ...
-                + dB * B * [K(1,:)*(x-xd)*den2; K(2,:)*(x-xd)*den1] ...
+xd = [0;6];
+den1 = 1 + ((K(1,:)*(x-xd)-um)/(2*umax))^2;
+den2 = 1 + ((K(2,:)*(x-xd))/(2*vmax))^2;
+control_const = (dB * (A*x + B*(um+E*d)) * den1*den2 ...
+                + dB * B * [(K(1,:)*(x-xd))*den2; K(2,:)*(x-xd)*den1] ...
                 + gamma*Barrier * den1*den2 ) ...
                 - (s3*safe_set1 + s4*(dumax-d)*(d-dumin));
-            
-control_const = (dB * (A*x + B*(u./[den1;den2]+E*d)) * den1*den2 ...
-                + gamma*Barrier * den1*den2 ) ...
-                - (s3*safe_set1 + s4*(dumax-d)*(d-dumin));
-            
-control_const = (dB * (A*x + B*(u./[den1;den2])) * den1*den2 ...
-                + gamma*Barrier * den1*den2 ) ...
-                - (s3*safe_set1);
             
 % constraint = [ k>=0; sos(nept_const); sos(init_const); sos(safe_const); sos(control_const);...
 %               sos(B_hat); sos(s0); sos(s1); sos(s2);sos(s3);sos(s4);];% sos(s3); sos(s4)];
           
-constraint = [ k>=0; sos(nept_const); sos(safe_const1); sos(control_const);...
+constraint = [ k>=0; sos(nept_const); sos(safe_const1);sos(control_const)...
                sos(B_hat); sos(s0); sos(s21); sos(s3); sos(s4)];
 
-constraint = [ k>=0; sos(nept_const); sos(safe_const1); sos(control_const);...
-               sos(B_hat); sos(s0); sos(s21); sos(s3)];
-
-% constraint = [ k>=0; sos(nept_const); sos(safe_const1);...
-%                sos(B_hat); sos(s0); sos(s21)];
-
-
 obj = [];
-variables = [k;coefB;c0;c21;c3];%;c4];%;c5];
+variables = [k;coefB;c0;c21;c3;c4];%;c5];
 %variables = [k;coefB];
 [sol,v,Q,res] = solvesos(constraint, obj, opts, variables);
 
@@ -124,7 +106,7 @@ fcontour(str2sym(sdisplay(initial_X)), range, '-g', 'LevelList', [0])
 grid on;
 axis equal;
 % Substitute variables with values
-B_plot = replace(Barrier, [k; coefB; vx; vy], [value(k); value(coefB); 0; 0]);
+B_plot = replace(Barrier, [k; coefB], [value(k); value(coefB)]);
 fb = matlabFunction(str2sym(sdisplay(B_plot)));
 fcontour(str2sym(sdisplay(B_plot)), range, '-b', 'LevelList', [0])%, [-50 50 -50 50])
 
@@ -136,59 +118,60 @@ while true
     expand_const = k - old_k >= 0;
     
     [s01, c01, m01] = polynomial(x,2);
-    [s02, c02, m02] = polynomial(x,2);
     safe_const1 = -(k - old_Bhat + eps) + s01*safe_set1;
-    safe_const2 = -(k - old_Bhat + eps) + s02*safe_set2;
+
     
     dB = jacobian(k - old_Bhat, x);
-    [ux_input, cx_input, mx_input] = polynomial(x,2);
-    [uy_input, cy_input, my_input] = polynomial(x,2);
+    [ux_input, cx_input, mx_input] = polynomial([x;d],2);
+    [uy_input, cy_input, my_input] = polynomial([x;d],2);
     
     [s11, c11, m11] = polynomial([x;d],2);
     [s12, c12, m12] = polynomial([x;d],2);
-    control_const = -(dB * (A*x + B*([ux_input;uy_input] + E*d))...
-                    + gamma*Barrier + eps)...
-                    + (s11*safe_set1 + s12*(dumax-d)*(d-dumin));
+    control_const = (dB * (A*x + B*([ux_input;uy_input] + E*d))...
+                    + gamma*(k-old_Bhat) + eps)...
+                    - (s11*safe_set1 + s12*(dumax-d)*(d-dumin));
     
     [s21, c21, m21] = polynomial([x;d],2);
     [s22, c22, m22] = polynomial([x;d],2);
-    ux_const1 = (ux_input - uxmin) - (s21*safe_set2 + s22*(dumax-d)*(d-dumin));
+    ux_const1 = (ux_input - umin) - (s21*safe_set1 + s22*(dumax-d)*(d-dumin));
     
     [s31, c31, m31] = polynomial([x;d],2);
     [s32, c32, m32] = polynomial([x;d],2);
-    ux_const2 = (-ux_input + uxmax) - (s31*safe_set2 + s32*(dumax-d)*(d-dumin));
+    ux_const2 = (-ux_input + umax) - (s31*safe_set1 + s32*(dumax-d)*(d-dumin));
     
     [s41, c41, m41] = polynomial([x;d],2);
     [s42, c42, m42] = polynomial([x;d],2);
-    uy_const1 = (uy_input - uymin) - (s41*safe_set2 + s42*(dumax-d)*(d-dumin));
+    uy_const1 = (uy_input + vmax) - (s41*safe_set1 + s42*(dumax-d)*(d-dumin));
     
     [s51, c51, m51] = polynomial([x;d],2);
     [s52, c52, m52] = polynomial([x;d],2);
-    uy_const2 = (-uy_input + uymax) - (s51*safe_set2 - s52*(dumax-d)*(d-dumin));
+    uy_const2 = (-uy_input + vmax) - (s51*safe_set1 + s52*(dumax-d)*(d-dumin));
     
-    constraint = [k >= 0; expand_const; sos(safe_const1); sos(safe_const2); sos(control_const);...
-                  sos(ux_const1);sos(ux_const2);...
-                  sos(uy_const1);sos(uy_const2);...
-                  sos(s01); sos(s02); sos(s11); sos(s12);...
-                  sos(s21); sos(s22);...
-                  sos(s31); sos(s32);...
-                  sos(s41); sos(s42);...
-                  sos(s51); sos(s52)];
+    constraint = [k >= 0; sos(safe_const1); sos(control_const);...
+%                   sos(ux_const1);sos(ux_const2);...
+%                   sos(uy_const1);sos(uy_const2);...
+                  sos(s01); sos(s11); sos(s12)];%;...
+%                   sos(s21); sos(s22);...
+%                   sos(s31); sos(s32);...
+%                   sos(s41); sos(s42);...
+%                   sos(s51); sos(s52)];
     obj = [-k];
-    variables = [k;c01;c02;c11;c12;c21;c22;c31;c32;c41;c42;c51;c52];
+    variables = [k;c01;c11;c12;c21;c22;c31;c32;c41;c42;c51;c52];
+    variables = [k;c01;c11;c12];
     [sol,v,Q,res] = solvesos(constraint, obj, opts, variables);
     
     figure
     range = [-35 20 -20 35];
     hold on
-    fcontour(str2sym(sdisplay(safe_set1)), range, '-r', 'LevelList', [0])
-    fcontour(str2sym(sdisplay(safe_set2)), range, '-r', 'LevelList', [0])
-    fcontour(str2sym(sdisplay(init_set)), range, '-k', 'LevelList', [0])
+    fcontour(str2sym(sdisplay(safe_set)), range, '-r', 'LevelList', [0])
+    fcontour(str2sym(sdisplay(safe_set1)), range, '-k', 'LevelList', [0])
+    %fcontour(str2sym(sdisplay(safe_set2)), range, '-r', 'LevelList', [0])
+    %fcontour(str2sym(sdisplay(init_set)), range, '-k', 'LevelList', [0])
     fcontour(str2sym(sdisplay(initial_X)), range, '-g', 'LevelList', [0])
     grid on;
     axis equal;
     % Substitute variables with values
-    B_plot = replace(k-B_hat, [k; coefB; vx; vy], [value(k); value(coefB); 0; 0]);
+    B_plot = replace(k-B_hat, [k; coefB], [value(k); value(coefB)]);
     fb = matlabFunction(str2sym(sdisplay(B_plot)));
     fcontour(str2sym(sdisplay(B_plot)), range, '-b', 'LevelList', [0])%, [-50 50 -50 50])
 
@@ -198,7 +181,7 @@ while true
     [B_hat, coefB, mon] = polynomial(x,2);
     
     [s0, c0, m0] = polynomial(x,2);
-    safe_const = -(old_Bhat - k) + s0*safe - eps;
+    safe_const = -(old_Bhat - k) + s0*safe_set - eps;
     
     [s1, c1, m1] = polynomial(x,2);
     Barrier = B_hat - k;
@@ -234,6 +217,7 @@ hold on
 % Substitute variables with values
 B_plot = replace(B_hat-k, [k; coefB], [clean(value(k),1e-8); clean(value(coefB),1e-8)]);
 fcontour(str2sym(sdisplay(B_plot)), range, '-b', 'LevelList', [0])
+
 
 
 
